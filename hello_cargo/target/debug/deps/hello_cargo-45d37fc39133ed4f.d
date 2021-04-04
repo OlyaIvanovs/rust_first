d36d63f274488952f2bf1d30e60cad76778197a8
olya <olya@ivanovs.info>
2021-04-05 07:47:49 +1200

    Add workspace

--- hello_cargo/target/debug/deps/hello_cargo-45d37fc39133ed4f.d
+++
@@ -1,5 +0,0 @@
-/home/olya/projects/rust_first/hello_cargo/target/debug/deps/hello_cargo-45d37fc39133ed4f: src/main.rs
-
-/home/olya/projects/rust_first/hello_cargo/target/debug/deps/hello_cargo-45d37fc39133ed4f.d: src/main.rs
-
-src/main.rs:
--- hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/home/olya/projects/rust_first/hello_cargo/target/debug/hello_cargo: /home/olya/projects/rust_first/hello_cargo/src/main.rs
