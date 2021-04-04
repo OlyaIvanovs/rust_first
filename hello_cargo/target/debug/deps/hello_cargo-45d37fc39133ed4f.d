394c2393f402a12ae8d67a64912b7a74bb05973b
olya <olya@ivanovs.info>
2021-04-04 19:10:00 +1200

    First cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-45d37fc39133ed4f.d
@@ -0,0 +1,5 @@
+/home/olya/projects/rust_first/hello_cargo/target/debug/deps/hello_cargo-45d37fc39133ed4f: src/main.rs
+
+/home/olya/projects/rust_first/hello_cargo/target/debug/deps/hello_cargo-45d37fc39133ed4f.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/olya/projects/rust_first/hello_cargo/target/debug/hello_cargo: /home/olya/projects/rust_first/hello_cargo/src/main.rs
